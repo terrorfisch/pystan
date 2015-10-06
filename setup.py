b90272ef0044ec347de2fae837ca54fb9ff4a192
Daniel Lee <bearlee@alum.mit.edu>
2015-10-06 11:41:55 -0400

    Trying to fix travis-ci error

--- setup.py
+++ setup.py
@@ -99,8 +99,8 @@ from distutils.extension import Extension
 
 stan_include_dirs = ["pystan/stan/src",
                      "pystan/stan/lib/stan_math/",
-                     "pystan/stan/lib/stan_math/lib/eigen_3.2.4",
-                     "pystan/stan/lib/stan_math/lib/boost_1.58.0"]
+                     "pystan/stan/lib/stan_math/lib/eigen_3.2.4/",
+                     "pystan/stan/lib/stan_math/lib/boost_1.58.0/"]
 stan_macros = [
     ('BOOST_RESULT_OF_USE_TR1', None),
     ('BOOST_NO_DECLTYPE', None),
